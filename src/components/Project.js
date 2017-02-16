c5be2696701c16db164d7a96a2937cd894ecc481
Nik Sudan <nik.sudan@digital-results.com>
2017-02-16 12:02:22 +0000

    Able to add links to projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -6,6 +6,15 @@ const Project = ({ data }) => (
     <h4>{data.date}</h4>
     <h1>{data.name}</h1>
     <p>{data.description}</p>
+    {data.links && 
+      <ul>
+        {data.links.map((link, index) => (
+          <li key={index}>
+            <a target="_blank" href={link.link}>{link.name}</a>
+          </li>
+        ))}
+      </ul>
+    }
   </div>
 );
 
