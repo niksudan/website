ee1034440cb69d928aa06a921c533a41d7422ac1
Nik Sudan <nik.sudan@digital-results.com>
2017-02-27 17:14:43 +0000

    Handle back button routing

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import thunkMiddleware from 'redux-thunk';
 import { Router, Route, Redirect, browserHistory } from 'react-router';
 import { routerMiddleware } from 'react-router-redux';
 import * as reducers from './reducers';
+import { openInfobox } from './actions/infobox';
+import { selectProject, unselectProject } from './actions/projects';
 import App from './components/App';
 import './index.css';
 
@@ -17,9 +19,34 @@ const store = createStore(
   )
 );
 
+const update = () => {
+  const projects = store.getState().projects;
+  const dispatch = store.dispatch;
+  const path = window.location.pathname;
+
+  // Attempt to find and appropriate modal via pathname
+  console.log('ping');
+  if (!projects.isOpen && path !== '/') {
+    let found = false;
+    if (path === '/about') {
+      dispatch(openInfobox());
+      found = true;
+    } else {
+      projects.items.forEach((project, index) => {
+        if (`/${project.slug}` === path && !found) {
+          dispatch(selectProject(index));
+          found = true;
+        }
+      });
+    }
+  } else if (path === '/' && projects.isOpen) {
+    dispatch(unselectProject());
+  }
+};
+
 render(
   <Provider store={store}>
-    <Router history={browserHistory}>
+    <Router history={browserHistory} onUpdate={update}>
       <Route path="/" component={App} />
       <Route path="/:slug" component={App} />
       <Redirect from="/*" to="/" />
--- src/views/Index.js
+++ src/views/Index.js
@@ -7,67 +7,46 @@ import Panel from '../components/Panel';
 import Project from '../components/Project';
 import Modal from '../components/Modal';
 import { MOTION_MODAL } from '../constants/motion';
-import { selectProject, unselectProject, loadProjects } from '../actions/projects';
-import { openInfobox } from '../actions/infobox';
+import { unselectProject, loadProjects } from '../actions/projects';
 
-const Index = ({ dispatch, projects }) => {
-
-  // Attempt to find and appropriate modal via pathname
-  const path = window.location.pathname;
-  if (!projects.isOpen && path !== '/') {
-    let found = false;
-    if (path === '/about') {
-      dispatch(openInfobox());
-      found = true;
-    } else {
-      projects.items.forEach((project, index) => {
-        if (`/${project.slug}` === path && !found) {
-          dispatch(selectProject(index));
-          found = true;
-        }
-      });
-    }
-  }
-  
-  return (
-    <div className="app__view">
-      <InfiniteScroll
-        pageStart={projects.page}
-        loadMore={() => {
-          dispatch(loadProjects());
-        }}
-        hasMore={projects.items.length > projects.activeItems.length}
-        loader={<div />}
-        >
-        {projects.activeItems.map((project, index) => (
-          <Panel
-            key={index}
-            index={index}
-            data={project}
-            />
-        ))}
-      </InfiniteScroll>
-      <Motion
-        defaultStyle={{ transition: 0 }}
-        style={{ transition: spring(projects.isOpen ? 1 : 0, MOTION_MODAL) }}
-        >
-        {style =>
-          projects.current &&
-          <Modal
-            transition={style.transition}
-            handleClose={(e) => {
-              e.preventDefault();
-              dispatch(unselectProject());
-              browserHistory.push('/');
-            }}
-            >
-            <Project data={projects.current} />
-          </Modal>
-        }
-      </Motion>
-    </div>
-  );
-};
+const Index = ({ dispatch, projects }) => (
+  <div className="app__view">
+    <InfiniteScroll
+      pageStart={projects.page}
+      loadMore={() => {
+        dispatch(loadProjects());
+      }}
+      hasMore={projects.items.length > projects.activeItems.length}
+      loader={<div />}
+      >
+      {projects.activeItems.map((project, index) => (
+        <Panel
+          key={index}
+          index={index}
+          data={project}
+          />
+      ))}
+    </InfiniteScroll>
+    <Motion
+      defaultStyle={{ transition: 0 }}
+      style={{ transition: spring(projects.isOpen ? 1 : 0, MOTION_MODAL) }}
+      >
+      {style =>
+        projects.current &&
+        <Modal
+          transition={style.transition}
+          handleClose={(e) => {
+            e.preventDefault();
+            dispatch(unselectProject());
+            browserHistory.push('/');
+          }}
+          >
+          <Project data={projects.current} />
+        </Modal>
+      }
+    </Motion>
+  </div>
+);
 
 const mapStateToProps = (state) => ({
   projects: state.projects,
