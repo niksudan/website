8d13724a8245cead2f97e598fab243d9f3e8ac2b
Nik Sudan <nik.sudan@digital-results.com>
2017-03-03 13:33:35 +0000

    Fix Infobox rendering

--- src/components/InfoboxLarge.js
+++ src/components/InfoboxLarge.js
@@ -4,31 +4,30 @@ import Helmet from 'react-helmet';
 import './styles/InfoboxLarge.css';
 
 const InfoboxLarge = ({ infobox }) => (
-  <Helmet title="About">
-    <div className="infobox-large">
-      <div className="infobox-large__avatar">
-        <img src={infobox.data.avatar} alt="Avatar" />
-      </div>
-      <div className="infobox-large__heading">
-        <h1>{infobox.data.name}</h1>
-        <p>{infobox.data.description}</p>
-      </div>
-      <div className="infobox-large__content">
-        {infobox.data.links.map((section, index) => (
-          <div key={index}>
-            <h3>{section.name}</h3>
-            <ul>
-              {section.links.map((link, subindex) => (
-                <li key={subindex}>
-                  <a href={link.link} target="_blank">{link.name}</a>
-                </li>
-              ))}
-            </ul>
-          </div>
-        ))}
-      </div>
+  <div className="infobox-large">
+    <Helmet title="About" />
+    <div className="infobox-large__avatar">
+      <img src={infobox.data.avatar} alt="Avatar" />
     </div>
-  </Helmet>
+    <div className="infobox-large__heading">
+      <h1>{infobox.data.name}</h1>
+      <p>{infobox.data.description}</p>
+    </div>
+    <div className="infobox-large__content">
+      {infobox.data.links.map((section, index) => (
+        <div key={index}>
+          <h3>{section.name}</h3>
+          <ul>
+            {section.links.map((link, subindex) => (
+              <li key={subindex}>
+                <a href={link.link} target="_blank">{link.name}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  </div>
 );
 
 const mapStateToProps = (state) => ({
