a0cd07fcfc461bbf5436a69c3a09383f02db170e
Nik Sudan <nik.sudan@digital-results.com>
2017-05-12 15:47:26 +0100

    Add tag icons

--- src/components/TagList.js
+++ src/components/TagList.js
@@ -1,10 +1,40 @@
 import React from 'react';
+import FontAwesome from 'react-fontawesome';
 import './styles/TagList.css';
 
 const TagList = ({ tags }) => (
   <div className="taglist">
     {tags.map((tag) => (
-      <h6 className={`tag tag--${tag}`}>{tag}</h6>
+      <h6 className={`tag tag--${tag}`}>
+        {tag === 'game' &&
+          <FontAwesome name="gamepad" />
+        }
+        {tag === 'cancelled' &&
+          <FontAwesome name="ban" />
+        }
+        {tag === 'article' &&
+          <FontAwesome name="book" />
+        }
+        {tag === 'open-source' &&
+          <FontAwesome name="code-fork" />
+        }
+        {tag === 'music' &&
+          <FontAwesome name="music" />
+        }
+        {tag === 'podcast' &&
+          <FontAwesome name="podcast" />
+        }
+        {tag === 'video' &&
+          <FontAwesome name="youtube-play" />
+        }
+        {tag === 'collaboration' &&
+          <FontAwesome name="users" />
+        }
+        {tag === 'in-development' &&
+          <FontAwesome name="edit" />
+        }
+        {tag}
+      </h6>
     ))}
   </div>
 );
