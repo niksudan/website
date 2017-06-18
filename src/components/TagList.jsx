32a8fa15f063bfd7e71433a4fafb2fd9b4e2773c
Nik Sudan <nik.sudan@digital-results.com>
2017-06-18 12:03:34 +0100

    Use JSX extension for React files

--- src/components/TagList.jsx
+++ src/components/TagList.jsx
@@ -5,7 +5,7 @@ import './styles/TagList.css';
 const TagList = ({ tags }) => (
   <div className="taglist">
     {tags.map((tag) => (
-      <h6 className={`tag tag--${tag}`}>
+      <h6 key={tag} className={`tag tag--${tag}`}>
         {tag === 'game' &&
           <FontAwesome name="gamepad" />
         }
