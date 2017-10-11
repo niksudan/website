f19da050e0a387e396ef641373d69e80dd93bca1
Nik Sudan <nik.sudan@digital-results.com>
2017-10-11 21:47:37 +0100

    Optimise public directory for serving

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,56 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const babili = require('gulp-babili');
+const cleanCSS = require('gulp-clean-css');
+const rimraf = require('rimraf');
+const browserSync = require('browser-sync').create();
 
-gulp.task('styles', () => 
-  gulp.src('./scss/**/*.scss')
+gulp.task('clean', (cb) =>
+  rimraf('./dist', cb)
+);
+
+gulp.task('styles', () =>
+  gulp.src('./src/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('./dist'))
 );
 
-gulp.task('watch', () => 
-  gulp.watch('./scss/**/*.scss', ['styles'])
+gulp.task('scripts', () =>
+  gulp.src([
+    './node_modules/tippy.js/dist/tippy.min.js',
+    './node_modules/vivus/dist/vivus.min.js',
+    './node_modules/smooth-scroll/dist/js/smooth-scroll.min.js',
+    './src/**/*.js',
+  ])
+    .pipe(concat('bundle.js'))
+    .pipe(babili({
+      mangle: {
+        keepClassName: true
+      }
+    }))
+    .pipe(gulp.dest('./dist/'))
 );
 
-gulp.task('default', ['styles']);
+gulp.task('html', () =>
+  gulp.src('src/**/*.html')
+    .pipe(gulp.dest('./dist'))
+);
+
+gulp.task('default', ['clean'], () => {
+  gulp.start('styles');
+  gulp.start('scripts');
+  gulp.start('html');
+});
+
+gulp.task('watch', ['default'], () => {
+  browserSync.init({
+    server: {
+        baseDir: "./dist"
+    }
+  });
+  gulp.watch('./src/**/*.scss', ['styles']).on('change', browserSync.stream);
+  gulp.watch('./src/**/*.js', ['scripts']).on('change', browserSync.reload);
+  gulp.watch('./src/**/*.html', ['html']).on('change', browserSync.reload);
+});
