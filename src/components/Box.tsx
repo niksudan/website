8ac41e2a6cbc3bf77053a87a40d2ede42deb7b99
Nik Sudan <niksudan@gmail.com>
2019-07-15 22:22:24 +0100

    Add that juicy logo animation back again

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -2,10 +2,21 @@ import * as React from 'react';
 
 interface Props {
   children: any;
+  index?: number;
 }
 
 export default class Box extends React.Component<Props> {
+  static defaultProps = {
+    index: 0,
+  };
+
   render() {
-    return <div className="box">{this.props.children}</div>;
+    return (
+      <div
+        className="box"
+        style={{ animationDelay: `${this.props.index * 0.2}s` }}>
+        {this.props.children}
+      </div>
+    );
   }
 }
